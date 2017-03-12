39704700eaefb7860bf891a3d184c95b4ef727e4
Duncan Pulsifer <duncanpulsifer@gmail.com>
2017-03-11 20:06:37 -0400

    Cleaned up DetailActivity UI and added back button

--- app/src/main/java/cs/dal/weatherapp/DetailActivity.java
+++ app/src/main/java/cs/dal/weatherapp/DetailActivity.java
@@ -1,7 +1,11 @@
 package cs.dal.weatherapp;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import cs.dal.weatherapp.weather.ForecastEntry;
@@ -13,12 +17,15 @@ public class DetailActivity extends AppCompatActivity {
     TextView detailLocation;
     TextView detailTitle;
     TextView detailInfo;
+    Button backButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Typeface font = Typeface.createFromAsset( getAssets(), "fontawesome-webfont.ttf" );
 
         detailLocation = (TextView)findViewById(R.id.detailLocation);
         detailTitle = (TextView)findViewById(R.id.detailTitle);
@@ -30,6 +37,17 @@ public class DetailActivity extends AppCompatActivity {
         detailLocation.setText(currentForecast.getLocation());
         detailTitle.setText(currentEntry.getTitle().split("\\:", 2)[0]);
         detailInfo.setText(currentEntry.getSummary());
+
+        backButton = (Button)findViewById(R.id.detailBackButton);
+        backButton.setTypeface(font);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
